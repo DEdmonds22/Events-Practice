7103ce456678f5454d778cec47df65cca191b690
DEdmonds22 <ddedmonds22@gmail.com>
2023-06-14 16:30:32 -0400

    What are DOM Event?

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// WHAT ARE DOM EVENTS? //
+    /* DOM events are the bedrock of interactivity on web pages. */
+    /* DOM events enable us as developers to implement event-driven programming. This programming paradigm is such that much of our code runs in response to events being triggered during run-time. */
+    /* Lots of events are being generated within the browser, for example, when:
+        + A user moves or clicks the mouse.
+        + A user presses a key.
+        + When a form is submitted.
+        + When the page has finished loading or has been resized.
+        + ect. */
